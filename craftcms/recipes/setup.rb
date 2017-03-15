faba68707222fcff978112ca20471fe58177d212
Tim Heider <tim@preludevoice.com>
2017-03-15 13:55:31 -0700

    Ubuntu 14 with php

--- craftcms/recipes/setup.rb
+++ craftcms/recipes/setup.rb
@@ -10,12 +10,4 @@ package "php" do
     action :install
 end
 
-package "php5-mysql" do
-    action :install
-end
-
-package "mod_php5" do
-    action :install    
-end
-
 Chef::Log.info("-- SETUP COMPLETE")

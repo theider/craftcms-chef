e4a5f5ce1b48130c152220f6dd9443e04b04d52c
Tim Heider <tim@preludevoice.com>
2017-03-15 17:00:36 -0700

    apt recipe

--- craftcms/metadata.rb
+++ craftcms/metadata.rb
@@ -2,8 +2,8 @@ name "craftcms"
 description "CraftCMS Web Server HTTP"
 
 depends "apache2"
+depends "apt"
 depends "build-essential"
 depends "mysql"
-depends "php"
 
 supports "ubuntu"
--- craftcms/recipes/setup.rb
+++ craftcms/recipes/setup.rb
@@ -1,14 +1,12 @@
 Chef::Log.info("-- SETUP START")
 
 include_recipe "apache2"
+include_recipe "apt"
+
+apt_update
 
 apache_site "default" do
     enable true
-end
-
-package "php" do
-    action :install
-    version "7.0.4"
 end
 
 Chef::Log.info("-- SETUP COMPLETE")

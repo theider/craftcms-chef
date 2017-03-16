f82f9ea43ddd4ccc29e1cb7ba9273862313d6a1e
Tim Heider <tim@preludevoice.com>
2017-03-16 15:37:11 -0700

    show node

--- craftcms/recipes/deploy.rb
+++ craftcms/recipes/deploy.rb
@@ -3,8 +3,10 @@ Chef::Log.info("-- DEPLOY START")
 
 app = search("aws_opsworks_app").first
 
-Chef::Log.info("App short name is '#{app['shortname']}''")
-Chef::Log.info("App URL is '#{app['app_source']['url']}'")
+Chef::Log.info("App: '#{app['shortname']}''")
+Chef::Log.info("App URL: '#{app['app_source']['url']}'")
+
+Chef::Log.info("App node: '#{app.inspect}'")
 
 application '/srv/app' do
     # check out code

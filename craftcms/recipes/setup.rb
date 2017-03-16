69ba155cd632876a8b31e9bc7658fdcb4fd6b720
Tim Heider <tim@preludevoice.com>
2017-03-15 17:08:36 -0700

    apt recipe

--- craftcms/recipes/setup.rb
+++ craftcms/recipes/setup.rb
@@ -3,7 +3,9 @@ Chef::Log.info("-- SETUP START")
 include_recipe "apache2"
 include_recipe "apt"
 
-apt_update
+apt_update 'update' do
+  action :update
+end
 
 apache_site "default" do
     enable true

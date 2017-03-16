fe6df83fdc0191ebd74942b30523fd07cffab0e7
Tim Heider <tim@preludevoice.com>
2017-03-15 20:49:30 -0700

    git checkout in application

--- craftcms/recipes/deploy.rb
+++ craftcms/recipes/deploy.rb
@@ -5,12 +5,14 @@ app = search("aws_opsworks_app").first
 Chef::Log.info("App short name is '#{app['shortname']}''")
 Chef::Log.info("App URL is '#{app['app_source']['url']}'")
 
-# check out code
-git "/home/ubuntu" do
-    repository app['app_source']['url']
-    reference 'master'
-    action :sync
-    deploy_key app["app_source"]["ssh_key"]
+application '/home/ubuntu' do
+    # check out code
+    git "/home/ubuntu" do
+        repository app['app_source']['url']
+        reference 'master'
+        action :sync
+        deploy_key app["app_source"]["ssh_key"]
+    end
 end
 
 Chef::Log.info("-- DEPLOY COMPLETE")

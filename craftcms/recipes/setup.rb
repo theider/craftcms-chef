004f89bcb87149c04cd56cc65635dae75299bfb3
Tim Heider <tim@preludevoice.com>
2017-04-17 10:13:20 -0700

    symbolic link and install PHP with apache

--- craftcms/recipes/setup.rb
+++ craftcms/recipes/setup.rb
@@ -11,6 +11,14 @@ apache_site "default" do
     enable true
 end
 
-package ['php7.0', 'php-mbstring', 'php-mysql', 'php-curl', 'imagemagick php-imagick', 'php-mcrypt']
+package ['php7.0', 'php-mbstring', 'php-mysql', 'php-curl', 'imagemagick php-imagick', 'php-mcrypt', 'libphp7']
+
+directory '/var/www/html' do
+  action :delete
+end
+
+link '/var/www/html' do
+  to '/srv/app/public'
+end
 
 Chef::Log.info("-- SETUP COMPLETE")

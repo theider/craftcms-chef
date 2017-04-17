c02c416dfd0919adc188aeab6f745934048e4b20
Tim Heider <tim@preludevoice.com>
2017-04-17 10:30:30 -0700

    remove html folder

--- craftcms/recipes/setup.rb
+++ craftcms/recipes/setup.rb
@@ -15,10 +15,17 @@ package ['php7.0', 'php-mbstring', 'php-mysql', 'php-curl', 'imagemagick php-ima
 
 directory '/var/www/html' do
   action :delete
+  recursive true
 end
 
 link '/var/www/html' do
   to '/srv/app/public'
+end
+
+directory '/srv/app' do
+  owner 'www-data'
+  group 'www-data'
+  mode '0755'
 end
 
 Chef::Log.info("-- SETUP COMPLETE")

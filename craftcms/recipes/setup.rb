c32a4ab20a878e2a22cc2f404812d874cea30f3e
Tim Heider <tim@preludevoice.com>
2017-06-22 10:42:04 -0700

    enable default

--- craftcms/recipes/setup.rb
+++ craftcms/recipes/setup.rb
@@ -7,6 +7,10 @@ apt_update 'update' do
   action :update
 end
 
+apache_site "default" do
+    enable true
+end
+
 package ['php7.0', 'php-mbstring', 'php-mysql', 'php-curl', 'php-xml', 'php-simplexml', 'imagemagick php-imagick', 'php-mcrypt', 'libapache2-mod-php']
 
 template "/etc/apache2/apache2.conf" do

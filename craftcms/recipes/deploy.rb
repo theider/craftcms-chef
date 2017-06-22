a80197dbee835e663a56c4b33783a62b77c53fff
Tim Heider <tim@preludevoice.com>
2017-06-22 07:34:42 -0700

    vhost deployment

--- craftcms/recipes/deploy.rb
+++ craftcms/recipes/deploy.rb
@@ -36,85 +36,85 @@ if op_command['type'] == 'deploy'
     end
 end     
 
-app = search("aws_opsworks_app").first
+# app = search("aws_opsworks_app").first
 
-data_source = app['data_sources'][0]
-rds_instance = search("aws_opsworks_rds_db_instance").first
+# data_source = app['data_sources'][0]
+# rds_instance = search("aws_opsworks_rds_db_instance").first
 
-Chef::Log.info("RDS info=#{rds_instance.inspect}")
-Chef::Log.info("App: '#{app['shortname']}''")
-Chef::Log.info("App URL: '#{app['app_source']['url']}'")
-Chef::Log.info("App node: '#{app.inspect}'")
+# Chef::Log.info("RDS info=#{rds_instance.inspect}")
+# Chef::Log.info("App: '#{app['shortname']}''")
+# Chef::Log.info("App URL: '#{app['app_source']['url']}'")
+# Chef::Log.info("App node: '#{app.inspect}'")
 
-user 'create_user' do
-    comment 'realise'
-    username app_domain_name
-    group app_domain_name
-    manage_home true
-    home '/home/' + app_domain_name
-    shell '/bin/bash'
-end
+# user 'create_user' do
+#     comment 'realise'
+#     username app_domain_name
+#     group app_domain_name
+#     manage_home true
+#     home '/home/' + app_domain_name
+#     shell '/bin/bash'
+# end
 
-application '/srv/app' do
-    # check out code
-    git "/srv/app" do
-        repository app['app_source']['url']
-        reference 'master'
-        action :sync
-        deploy_key app["app_source"]["ssh_key"]
-    end   
-end
+# application '/srv/app' do
+#     # check out code
+#     git "/srv/app" do
+#         repository app['app_source']['url']
+#         reference 'master'
+#         action :sync
+#         deploy_key app["app_source"]["ssh_key"]
+#     end   
+# end
 
-link '/var/www/html' do
-  to '/srv/app/craftcms/public'
-end
+# link '/var/www/html' do
+#   to '/srv/app/craftcms/public'
+# end
 
-directory '/srv/app' do
-  owner 'www-data'
-  group 'www-data'
-  mode '0755'
-  recursive true
-end
+# directory '/srv/app' do
+#   owner 'www-data'
+#   group 'www-data'
+#   mode '0755'
+#   recursive true
+# end
 
-template "/srv/app/craftcms/craft/config/db.php" do
-  source "db.php.erb"
-  mode 0660
+# template "/srv/app/craftcms/craft/config/db.php" do
+#   source "db.php.erb"
+#   mode 0660
 
-  variables(
-    :host =>     (rds_instance[:address]),
-    :user =>     (rds_instance[:db_user]),
-    :password => (rds_instance[:db_password]),
-    :database_name =>  (data_source[:database_name])      
-  )
-end
+#   variables(
+#     :host =>     (rds_instance[:address]),
+#     :user =>     (rds_instance[:db_user]),
+#     :password => (rds_instance[:db_password]),
+#     :database_name =>  (data_source[:database_name])      
+#   )
+# end
 
-template "/srv/app/craftcms/craft/config/general.php" do
-  source "general.php.erb"
-  mode 0660
+# template "/srv/app/craftcms/craft/config/general.php" do
+#   source "general.php.erb"
+#   mode 0660
 
-  variables(
-    :site_url =>     (app['domains'][0] rescue nil),      
-  )
-end
+#   variables(
+#     :site_url =>     (app['domains'][0] rescue nil),      
+#   )
+# end
 
-template "/srv/app/craftcms/public/.htaccess" do
-  source "htaccess.erb"
-  mode 0660
-end
+# template "/srv/app/craftcms/public/.htaccess" do
+#   source "htaccess.erb"
+#   mode 0660
+# end
 
-execute "remove htaccess file" do
-    action :run
-    command "rm -f  /srv/app/craftcms/public/htaccess"
-end
+# execute "remove htaccess file" do
+#     action :run
+#     command "rm -f  /srv/app/craftcms/public/htaccess"
+# end
 
-execute "update owner permission" do
-    action :run
-    command "chown -R www-data /srv/app"
-end
+# execute "update owner permission" do
+#     action :run
+#     command "chown -R www-data /srv/app"
+# end
 
-execute "update group permission" do
-    action :run
-    command "chgrp -R www-data /srv/app"
-end
+# execute "update group permission" do
+#     action :run
+#     command "chgrp -R www-data /srv/app"
+# end
 
 Chef::Log.info("-- DEPLOY COMPLETE")
